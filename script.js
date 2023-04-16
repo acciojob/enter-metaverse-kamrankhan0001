dfda143a03075bd30d49d62a26678851665f3b48
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-16 19:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function enterMsg(){
+	var ans=document.getElementById("status");
+	ans.innerText="Entered Metaverse"
+	var head=document.getElementById("head");
+ans.innerText="";
+	head.innerText="Entered Metaverse";
+}

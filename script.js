b9578c7fa377f21afd17bcc1b99b925f7eaa386f
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-16 20:42:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //your JS code here. If required.
-function enterMsg(){
-	var ans=document.getElementById("status");
-	ans.innerText="Entered Metaverse"
-	var head=document.getElementById("head");
-ans.innerText="";
-	head.innerText="Entered Metaverse";
+// function enterMsg(){
+// 	var ans=document.getElementById("status");
+// 	ans.innerText="Entered Metaverse"
+// 	var head=document.getElementById("head");
+// ans.innerText="";
+// 	head.innerText="Entered Metaverse";
+// }
+// 
+function changeText() {
+  // Get the p tag by its id
+  var status = document.getElementById("status");
+
+  // Change the text of p tag to "Entered Metaverse"
+  status.innerHTML = "Entered Metaverse";
 }

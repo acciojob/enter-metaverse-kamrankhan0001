892d8d9128511de0e1b0465e05d894d1fcce5c5e
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-30 09:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function changeText() {
   var status = document.getElementById("status");
 
   // Change the text of p tag to "Entered Metaverse"
-  status.innerHTML = "Entered Metaverse";
+  status.innerHTML = "<h1>Entered Metaverse</h1>";
 }
